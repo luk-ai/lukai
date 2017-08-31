3b0899d64f56bb10e3c5ab15e5ea03c5be8b6496
Tristan Rice <rice@fn.lc>
2017-08-30 18:27:48 -0700

    py: added protobuf packages to setup.py

--- py/setup.py
+++ py/setup.py
@@ -1,8 +1,13 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+import os
+
+packages = find_packages('.')
+
+packages += ['lukai.proto.{}'.format(pkg) for pkg in next(os.walk('lukai/proto'))[1]]
 
 setup(
     name='lukai',
-    packages=['lukai'],
+    packages=packages,
     version='0.1',
     description='Luk.ai management library',
     install_requires=['grpcio', 'six'],
