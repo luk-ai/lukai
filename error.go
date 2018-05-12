ec6779dbb0f6110a966babc45a69e0b7d5f6bc4f
Tristan Rice <rice@fn.lc>
2018-05-12 12:19:39 -0700

    errors: wrap training and example errors

--- error.go
+++ error.go
@@ -1,5 +1,7 @@
 package lukai
 
+import "github.com/pkg/errors"
+
 // TrainingError returns the last training error that occured and clears it.
 // I.e. calling TrainingError twice if there is only one error will return the
 // error the first time and then nil afterwards.
@@ -8,9 +10,11 @@ func (mt *ModelType) TrainingError() error {
 	mt.training.Lock()
 	defer mt.training.Unlock()
 
-	err := mt.training.err
-	mt.training.err = nil
-	return err
+	if err := mt.training.err; err != nil {
+		mt.training.err = nil
+		return errors.Wrap(err, "training error")
+	}
+	return nil
 }
 
 // ExamplesError returns the last examples saving error that occured and clears
@@ -21,7 +25,9 @@ func (mt *ModelType) ExamplesError() error {
 	mt.examplesMeta.Lock()
 	defer mt.examplesMeta.Unlock()
 
-	err := mt.examplesMeta.err
-	mt.examplesMeta.err = nil
-	return err
+	if err := mt.examplesMeta.err; err != nil {
+		mt.examplesMeta.err = nil
+		return errors.Wrap(err, "example error")
+	}
+	return nil
 }
