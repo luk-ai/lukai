2063c8955c58688e01eb5aa8eaeb8c6ede56ba73
Tristan Rice <rice@fn.lc>
2018-05-12 15:44:33 -0700

    net: added URL on load error

--- net/load.go
+++ net/load.go
@@ -11,10 +11,10 @@ import (
 func OpenModel(url string) (io.ReadCloser, error) {
 	resp, err := http.Get(url)
 	if err != nil {
-		return nil, err
+		return nil, errors.Wrap(err, url)
 	}
 	if resp.StatusCode != http.StatusOK {
-		return nil, errors.Errorf("expected status %+v; got %+v", http.StatusOK, resp.StatusCode)
+		return nil, errors.Errorf("%q: expected status %+v; got %+v", url, http.StatusOK, resp.StatusCode)
 	}
 	return resp.Body, nil
 }
