76c764a0c429396b5a68958172c605efd5e02bd1
Tristan Rice <rice@fn.lc>
2018-05-13 22:19:03 -0700

    testutil: failfast on test failure

--- testutil/testutil.go
+++ testutil/testutil.go
@@ -11,7 +11,14 @@ func SucceedsSoon(t *testing.T, f func() error) {
 	opts := backoff.NewExponentialBackOff()
 	opts.MaxElapsedTime = 15 * time.Second
 	opts.InitialInterval = 1 * time.Microsecond
-	if err := backoff.Retry(f, opts); err != nil {
+	fWrap := func() error {
+		// Fast fail if the test has already failed.
+		if t.Failed() {
+			return nil
+		}
+		return f()
+	}
+	if err := backoff.Retry(fWrap, opts); err != nil {
 		t.Fatal(err)
 	}
 }
