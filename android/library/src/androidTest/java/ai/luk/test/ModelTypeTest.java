8e164dd5edc6582fb7ba82e216729ad4af681701
Tristan Rice <rice@fn.lc>
2018-06-08 12:18:54 -0700

    android: ModelTypeTest test all types except string

--- android/library/src/androidTest/java/ai/luk/test/ModelTypeTest.java
+++ android/library/src/androidTest/java/ai/luk/test/ModelTypeTest.java
@@ -20,6 +20,7 @@ import java.io.File;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.List;
 
 import android.support.test.runner.AndroidJUnit4;
 
@@ -33,6 +34,37 @@ public class ModelTypeTest {
   public void log() throws Exception {
     File dir = tempdir.newFolder("lukai-java-test");
     ModelType mt = new ModelType("test", "test", dir.getPath());
+    mt.close();
+  }
+
+  @Test
+  public void logTypes() throws Exception {
+    File dir = tempdir.newFolder("lukai-java-test");
+    ModelType mt = new ModelType("test", "test", dir.getPath());
+
+    List<Tensor> tensors = Arrays.asList(
+      Tensor.create(1.0f, DataType.FLOAT),
+      Tensor.create(new float[]{1.0f}, DataType.FLOAT),
+      Tensor.create(1.0, DataType.DOUBLE),
+      Tensor.create(new double[]{1.0}, DataType.DOUBLE),
+      Tensor.create((int)1, DataType.INT32),
+      Tensor.create(new int[]{1}, DataType.INT32),
+      Tensor.create((long)1, DataType.INT64),
+      Tensor.create(new long[]{1}, DataType.INT64),
+      Tensor.create(true, DataType.BOOL),
+      Tensor.create(new boolean[]{true}, DataType.BOOL),
+      Tensor.create((byte)1, DataType.UINT8),
+      Tensor.create(new byte[]{1}, DataType.UINT8));
+
+    // TODO: support DataType.STRING
+    // Can work around by passing a byte array in most cases.
+
+    for (Tensor t : tensors) {
+      Map<String, Tensor> feeds = new HashMap<String, Tensor>();
+      feeds.put("a:0", t);
+      mt.log(feeds, Arrays.asList("target"));
+    }
+
     mt.close();
   }
 
